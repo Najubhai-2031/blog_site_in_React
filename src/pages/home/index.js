e91baac9e711b56fb7a2e88d0118571ee3343714
NarinAhirStack <narinb.stacksum@gmail.com>
2022-10-11 16:13:28 +0530

    User Data and Blog Data Merged

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,4 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import {
   addDoc,
   collection,
@@ -6,8 +7,10 @@ import {
   query,
   updateDoc,
   doc,
+  getDoc,
 } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
@@ -20,19 +23,46 @@ import "./style.css";
 const Home = () => {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
+  const [uId, setUId] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState([]);
 
   const navigate = useNavigate();
 
   const getBlogs = async () => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user !== null) {
+        setUId(user.uid);
+      } else {
+      }
+    });
     const querySnapshot = collection(db, "Blog");
     const data = await getDocs(query(querySnapshot, orderBy("timeStamp")));
-    setData([]);
-    const newData = data.docs.map((doc) => ({
-      ...doc.data(),
-    }));
-    const sorted = newData?.sort((a, b) => b.timeStamp - a.timeStamp);
-    setData(sorted);
+
+    const blogsUser = data.docs.map((blog) => {
+      return blog.data()?.uid;
+    });
+
+    Promise.all(blogsUser.map((userID) => getDoc(doc(db, "users", userID))))
+      .then((response) => {
+        const users = response?.map((user) => {
+          return user.data();
+        });
+        const blogData = data.docs
+          .map((blog) => {
+            const findUser = users.find(
+              (user) => blog?.data()?.uid === user.id
+            );
+            return { ...blog.data(), displayName: findUser.displayName };
+          })
+          .sort((a, b) => b.timeStamp - a.timeStamp);
+        setIsLoading(true);
+        setData(blogData);
+      })
+      .catch((err) => {
+        toast.error(err);
+      });
   };
 
   const handleSubmit = (event) => {
@@ -44,16 +74,17 @@ const Home = () => {
         Title: title,
         Description: desc,
         timeStamp: Date.now(),
+        uid: uId,
       }).then((docResponse) => {
         const docRef = doc(db, "Blog", docResponse?.id);
         updateDoc(docRef, {
           id: docResponse?.id,
         })
-          .then((response) => {
+          .then(() => {
             getBlogs();
           })
           .catch((err) => {
-            console.log("err", err);
+            toast.error(err);
           });
       });
       setTitle("");
@@ -66,74 +97,142 @@ const Home = () => {
     getBlogs();
   }, []);
 
-  return (
-    <React.Fragment>
-      <div>
-        <ToastContainer />
-        <div className="main">
-          <Form>
-            <div>
-              <h3>Blog</h3>
-            </div>
-            <div>
-              <Form.Group className="mb-3" controlId="formBasicTitle">
-                <Form.Control
-                  type="text"
-                  placeholder="Title"
-                  onChange={(e) => setTitle(e.target.value)}
-                  value={title}
-                />
-              </Form.Group>
-            </div>
-            <div>
+  if (!isLoading) {
+    return (
+      <React.Fragment>
+        <Container fluid>
+          <ToastContainer />
+          <div className="main">
+            <Form>
+              <div>
+                <h3>Blog</h3>
+              </div>
+              <div>
+                <Form.Group className="mb-3" controlId="formBasicTitle">
+                  <Form.Control
+                    type="text"
+                    placeholder="Title"
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                  />
+                </Form.Group>
+              </div>
+              <div>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlTextarea1"
+                >
+                  <Form.Control
+                    placeholder="Description"
+                    as="textarea"
+                    onChange={(e) => setDesc(e.target.value)}
+                    rows={3}
+                    value={desc}
+                  />
+                </Form.Group>
+              </div>
               <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Control
-                  placeholder="Description"
-                  as="textarea"
-                  onChange={(e) => setDesc(e.target.value)}
-                  rows={3}
-                  value={desc}
-                />
-              </Form.Group>
-            </div>
-            <Form.Group
-              className="d-grid gap-2"
-              controlId="formBasicCheckbox"
-            ></Form.Group>
-            <div className="d-grid gap-2">
-              <Button id="btn" variant="outline-primary" onClick={handleSubmit}>
-                Add Blog
-              </Button>
-            </div>
-          </Form>
-        </div>
-      </div>
-      {/* Map Method for data Showing */}
-      <div className="cards">
-        {data.map((item) => {
-          return (
-            <div className="cards-inner">
-              <Card style={{ width: "18rem", textAlign: "center" }}>
-                <Card.Body>
-                  <Card.Title>{item.Title}</Card.Title>
-                  <Card.Text>{item.Description}</Card.Text>
-                  <Button
-                    variant="primary me-2"
-                    onClick={() => navigate(`Blog/${item.id}`)}
-                  >
-                    Blog Details
-                  </Button>
-                </Card.Body>
-              </Card>
-            </div>
-          );
-        })}
-      </div>
-    </React.Fragment>
-  );
+                className="d-grid gap-2"
+                controlId="formBasicCheckbox"
+              ></Form.Group>
+              <div className="d-grid gap-2">
+                <Button
+                  id="btn"
+                  variant="outline-primary"
+                  onClick={handleSubmit}
+                >
+                  Add Blog
+                </Button>
+              </div>
+            </Form>
+          </div>
+        </Container>
+        <div className="text-center">Loading...</div>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <Container fluid>
+          <ToastContainer />
+          <div className="main">
+            <Form>
+              <div>
+                <h3>Blog</h3>
+              </div>
+              <div>
+                <Form.Group className="mb-3" controlId="formBasicTitle">
+                  <Form.Control
+                    type="text"
+                    placeholder="Title"
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                  />
+                </Form.Group>
+              </div>
+              <div>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlTextarea1"
+                >
+                  <Form.Control
+                    placeholder="Description"
+                    as="textarea"
+                    onChange={(e) => setDesc(e.target.value)}
+                    rows={3}
+                    value={desc}
+                  />
+                </Form.Group>
+              </div>
+              <Form.Group
+                className="d-grid gap-2"
+                controlId="formBasicCheckbox"
+              ></Form.Group>
+              <div className="d-grid gap-2">
+                <Button
+                  id="btn"
+                  variant="outline-primary"
+                  onClick={handleSubmit}
+                >
+                  Add Blog
+                </Button>
+              </div>
+            </Form>
+          </div>
+        </Container>
+        {/* Map Method for data Showing */}
+        <Container className="cards">
+          {data.map((item) => {
+            return (
+              <div className="cards-inner">
+                <Card style={{ width: "18rem", textAlign: "center" }}>
+                  <Card.Body>
+                    <Card.Text className="text">
+                      By:
+                      <b
+                        className="text"
+                        onClick={() => navigate(`AllProfiles/${item?.uid}`)}
+                      >
+                        {item.displayName}
+                      </b>
+                    </Card.Text>
+                    <Card.Title>{item.Title}</Card.Title>
+                    <Card.Text>{item.Description}</Card.Text>
+                    <Button
+                      variant="primary me-2"
+                      onClick={() => navigate(`Blog/${item?.id}`)}
+                    >
+                      Blog Details
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </div>
+            );
+          })}
+        </Container>
+      </React.Fragment>
+    );
+  }
 };
 
 export default Home;
