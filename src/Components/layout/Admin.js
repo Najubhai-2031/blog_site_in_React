6d66250d259f7ed6014246b1442562c250a04b88
NarinAhirStack <narinb.stacksum@gmail.com>
2022-10-20 12:01:10 +0530

    Added Sidebar

--- src/Components/layout/Admin.js
+++ src/Components/layout/Admin.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router";
 import AdminHeader from "../AdminHeader";
+import AdminSidebar from "../AdminSidebar";
 
 const Admin = () => {
   const user = useSelector((state) => state?.user?.user);
@@ -11,6 +12,7 @@ const Admin = () => {
       {user?.role === "admin" && user !== null ? (
         <>
           <AdminHeader />
+          <AdminSidebar />
           <Outlet />
         </>
       ) : (
